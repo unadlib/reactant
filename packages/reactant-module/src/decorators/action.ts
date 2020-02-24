933b7b53f102edfabe134d27d6bc484b7ecd1529
unadlib <unadlib@gmail.com>
2020-02-24 15:30:55 +0800

    fix(reactant-module): fix type

--- packages/reactant-module/src/decorators/action.ts
+++ packages/reactant-module/src/decorators/action.ts
@@ -3,26 +3,23 @@ import { produce, applyPatches } from 'immer';
 import { storeKey } from '../core/createStore';
 import { Service } from '../interfaces';
 
-let tempState: unknown;
+let tempState: any;
 
 export function action(
   target: object,
   key: string | symbol,
-  descriptor: TypedPropertyDescriptor<Function>
+  descriptor: TypedPropertyDescriptor<(...args: any[]) => void>
 ) {
   const fn = descriptor.value;
   if (typeof fn === 'undefined') {
     throw new Error(`${String(key)} decorate error with '@action'.`);
   }
-  const value = function(this: Service, ...args: unknown[]) {
+  const value = function(this: Service, ...args: any[]) {
     if (this[storeKey]) {
-      const state = produce(
-        this.state,
-        (draftState: Record<string, unknown>) => {
-          tempState = draftState;
-          fn.call({ ...this, state: draftState }, ...args);
-        }
-      );
+      const state = produce(this.state, (draftState: Record<string, any>) => {
+        tempState = draftState;
+        fn.call({ ...this, state: draftState }, ...args);
+      });
       tempState = undefined;
       this[storeKey].dispatch({
         type: this.name,
