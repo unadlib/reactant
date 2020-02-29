4bb2483ac8ba8376f2a9299d520c9f0f74ed2d63
unadlib <unadlib@gmail.com>
2020-02-29 21:18:25 +0800

    test(reactant-di): add `useValue` test

--- packages/reactant-di/test/index.test.ts
+++ packages/reactant-di/test/index.test.ts
@@ -243,3 +243,113 @@ test('base di with @optional, and resolve other module', () => {
   expect(fooBar.test).toEqual('test1');
   expect(fooBar.value).toEqual('bar');
 });
+
+test('base di with decoration for interface', () => {
+  interface FooInterface {
+    readonly test: string;
+  }
+
+  @injectable()
+  class Foo implements FooInterface {
+    public get test() {
+      return 'test';
+    }
+  }
+
+  interface BarInterface {
+    readonly test: string;
+    readonly value: string;
+  }
+
+  @injectable()
+  class Bar {
+    public constructor(
+      @optional('Foo') public foo: FooInterface,
+      @optional('Foo1') public foo1: FooInterface
+    ) {}
+
+    public get test() {
+      return this.foo && this.foo.test;
+    }
+
+    public get test1() {
+      return this.foo1 && this.foo1.test;
+    }
+
+    public get value() {
+      return 'bar';
+    }
+  }
+
+  @injectable()
+  class FooBar {
+    public constructor(@inject('Bar') public bar: BarInterface) {}
+
+    public get test() {
+      return this.bar.test;
+    }
+
+    public get value() {
+      return this.bar.value;
+    }
+  }
+
+  const fooBar = createContainer({
+    ServiceIdentifiers: new Map(),
+    modules: [
+      { provide: 'Foo', useClass: Foo },
+      { provide: 'Bar', useClass: Bar },
+    ],
+  }).get(FooBar);
+  expect(fooBar.test).toEqual('test');
+  expect((fooBar.bar as any).test1).toBeUndefined();
+  expect(fooBar.bar.test).toEqual('test');
+  expect(fooBar.value).toEqual('bar');
+});
+
+test('base di with useValue and useFactory config', () => {
+  interface FooInterface {
+    readonly test: string;
+  }
+
+  interface BarInterface {
+    readonly test: string;
+    readonly value: string;
+  }
+
+  @injectable()
+  class Bar {
+    public constructor(
+      @inject('Foo') public foo: FooInterface,
+      @optional('Foo1') public foo1: FooInterface,
+      @optional('Foo2') public foo2: FooInterface
+    ) {}
+
+    public get test() {
+      return this.foo && this.foo.test;
+    }
+
+    public get test1() {
+      return this.foo1 && this.foo1.test;
+    }
+
+    public get test2() {
+      return this.foo2 && this.foo2.test;
+    }
+
+    public get value() {
+      return 'bar';
+    }
+  }
+
+  const bar = createContainer({
+    ServiceIdentifiers: new Map(),
+    modules: [
+      { provide: 'Foo', useValue: { test: 'test' } },
+      { provide: 'Foo1', useValue: { test: 'test1' } },
+    ],
+  }).get(Bar);
+  expect(bar.test).toEqual('test');
+  expect(bar.test1).toEqual('test1');
+  expect(bar.test2).toBeUndefined();
+});
