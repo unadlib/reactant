37644c990d9ef25c23fafae80dd6f8eccc627158
unadlib <unadlib@gmail.com>
2020-03-27 01:26:29 +0800

    test(reactant-di): add `@inject` testing

--- packages/reactant-di/test/decorators/inject.test.ts
+++ packages/reactant-di/test/decorators/inject.test.ts
@@ -1,3 +1,101 @@
-test('', () => {
-  //
+import { injectable, createContainer, inject } from '../..';
+
+test('base di with @inject without args', () => {
+  @injectable()
+  class Foo {
+    public get test() {
+      return 'test';
+    }
+  }
+
+  @injectable()
+  class Bar {
+    constructor(@inject() public foo: Foo) {}
+
+    public get test() {
+      return this.foo.test;
+    }
+  }
+
+  const bar = createContainer({
+    ServiceIdentifiers: new Map(),
+  }).get(Bar);
+
+  expect(bar.test).toBe('test');
+});
+
+test('base di with @inject with itself', () => {
+  @injectable()
+  class Foo {
+    public get test() {
+      return 'test';
+    }
+  }
+
+  @injectable()
+  class Bar {
+    constructor(@inject(Foo) public foo: Foo) {}
+
+    public get test() {
+      return this.foo.test;
+    }
+  }
+
+  const bar = createContainer({
+    ServiceIdentifiers: new Map(),
+  }).get(Bar);
+
+  expect(bar.test).toBe('test');
+});
+
+test('base di with @inject with string token', () => {
+  @injectable()
+  class Foo {
+    public get test() {
+      return 'test';
+    }
+  }
+
+  @injectable()
+  class Bar {
+    constructor(@inject('Foo') public foo: Foo) {}
+
+    public get test() {
+      return this.foo.test;
+    }
+  }
+
+  const bar = createContainer({
+    ServiceIdentifiers: new Map(),
+    modules: [{ provide: 'Foo', useClass: Foo }],
+  }).get(Bar);
+
+  expect(bar.test).toBe('test');
+});
+
+test('base di with @inject with symbol token', () => {
+  @injectable()
+  class Foo {
+    public get test() {
+      return 'test';
+    }
+  }
+
+  const FooToken = Symbol('foo');
+
+  @injectable()
+  class Bar {
+    constructor(@inject(FooToken) public foo: Foo) {}
+
+    public get test() {
+      return this.foo.test;
+    }
+  }
+
+  const bar = createContainer({
+    ServiceIdentifiers: new Map(),
+    modules: [{ provide: FooToken, useClass: Foo }],
+  }).get(Bar);
+
+  expect(bar.test).toBe('test');
 });
