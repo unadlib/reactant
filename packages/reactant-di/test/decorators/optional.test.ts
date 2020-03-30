33430f4838711448d9b958644d15e96a2c1da503
unadlib <unadlib@gmail.com>
2020-03-30 23:56:13 +0800

    test(reactant-di): add `@optional` ut about inheritance

--- packages/reactant-di/test/decorators/optional.test.ts
+++ packages/reactant-di/test/decorators/optional.test.ts
@@ -101,3 +101,46 @@ test('base di with @optional, and resolve other module', () => {
   }).get(FooBar);
   expect(fooBar.bar.foo instanceof Foo1).toBeTruthy();
 });
+
+test('base di with @optional about inheritance', () => {
+  @injectable()
+  class Foo {}
+
+  @injectable()
+  class Foo1 {}
+
+  @injectable()
+  class Bar {
+    constructor(@optional() public foo: Foo) {}
+  }
+
+  @injectable()
+  class Bar1 extends Bar {
+    constructor(@optional() public foo: Foo, @optional() public foo1: Foo1) {
+      super(foo);
+    }
+  }
+  let bar1: Bar1;
+  bar1 = createContainer({
+    ServiceIdentifiers: new Map(),
+  }).get(Bar1);
+
+  expect(bar1.foo).toBeUndefined();
+  expect(bar1.foo1).toBeUndefined();
+
+  bar1 = createContainer({
+    ServiceIdentifiers: new Map(),
+    modules: [Foo],
+  }).get(Bar1);
+
+  expect(bar1.foo instanceof Foo).toBeTruthy();
+  expect(bar1.foo1).toBeUndefined();
+
+  bar1 = createContainer({
+    ServiceIdentifiers: new Map(),
+    modules: [Foo, Foo1],
+  }).get(Bar1);
+
+  expect(bar1.foo instanceof Foo).toBeTruthy();
+  expect(bar1.foo1 instanceof Foo1).toBeTruthy();
+});
