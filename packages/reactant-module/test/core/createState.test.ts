7ed74b8697f7799441594b350c8f4f634c4f54a9
unadlib <unadlib@gmail.com>
2020-04-03 00:09:05 +0800

    refactor(reactant-module): adjust createSelector type and add testing code

---
+++ packages/reactant-module/test/core/createState.test.ts
@@ -0,0 +1,38 @@
+import {
+  injectable,
+  createState,
+  createContainer,
+  createStore,
+  action,
+  state,
+  ReactantAction,
+} from '../..';
+
+test('`createSelector` with type', () => {
+  @injectable()
+  class Counter {
+    @state
+    count = createState<number, ReactantAction>((_state = 0, _action) =>
+      _action.type === (this as any).name ? _action.state.count : _state
+    );
+
+    @action
+    increase() {
+      this.count += 1;
+    }
+  }
+  const ServiceIdentifiers = new Map();
+  const modules = [Counter];
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules,
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const counter = container.get(Counter);
+  const store = createStore(container, ServiceIdentifiers);
+  expect(counter.count).toBe(0);
+  counter.increase();
+  expect(counter.count).toBe(1);
+});
