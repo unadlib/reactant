e3b5671ffcb640ef633f3a3da9d88ffacabde609
unadlib <unadlib@gmail.com>
2020-04-02 23:29:55 +0800

    test(reactant-module): adjust and add testing code

--- packages/reactant-module/test/index.test.ts
+++ packages/reactant-module/test/index.test.ts
@@ -3,7 +3,6 @@ import {
   multiInject,
   createContainer,
   createStore,
-  ServiceIdentifiersMap,
   action,
   state,
 } from '..';
@@ -14,9 +13,19 @@ test('base module with @state and @action', () => {
     @state
     count = 0;
 
+    @state
+    others = {
+      list: [] as number[],
+    };
+
     @action
     increase() {
       this.count += 1;
+    }
+
+    @action
+    add() {
+      this.others.list.push(this.others.list.length);
     }
   }
   const ServiceIdentifiers = new Map();
@@ -30,132 +39,148 @@ test('base module with @state and @action', () => {
   });
   const counter = container.get(Counter);
   const store = createStore(container, ServiceIdentifiers);
+  expect(counter.count).toBe(0);
+  expect(Object.values(store.getState())).toEqual([
+    { count: 0, others: { list: [] } },
+  ]);
   counter.increase();
-  // console.log(counter.count, store.getState());
+  expect(counter.count).toBe(1);
+  counter.add();
+  expect(counter.others.list).toEqual([0]);
+  expect(Object.values(store.getState())).toEqual([
+    { count: 1, others: { list: [0] } },
+  ]);
 });
 
-// test('module with multiple injection', () => {
-//   @injectable()
-//   class Foo {
-//     name = 'foo';
+test('module with stagedState about more effects', () => {
+  @injectable()
+  class Foo0 {
+    name = 'foo';
 
-//     state = { count: 1 };
+    @state
+    count0 = 1;
 
-//     @action
-//     increase() {
-//       this.state.count += 1;
-//     }
-//   }
+    @state
+    count1 = 1;
 
-//   @injectable()
-//   class FooTest {
-//     name = 'fooTest';
+    @action
+    increase() {
+      this.count0 += 1;
+    }
+  }
 
-//     state = { count: 1 };
-//   }
+  @injectable()
+  class Foo extends Foo0 {
+    count = 1;
 
-//   @injectable()
-//   class FooBar {
-//     constructor(@multiInject('FooToken') public foos: Foo[], public foo: Foo) {}
-//   }
+    add(count: number) {
+      this.count += count;
+    }
 
-//   const ServiceIdentifiers = new Map();
-//   const modules = [
-//     FooBar,
-//     { provide: 'FooToken', useClass: Foo },
-//     { provide: 'FooToken', useClass: Foo },
-//     { provide: 'FooToken', useClass: Foo },
-//     { provide: 'FooToken', useClass: FooTest },
-//   ];
-//   const container = createContainer({
-//     ServiceIdentifiers,
-//     modules,
-//     options: {
-//       defaultScope: 'Singleton',
-//     },
-//   });
-//   const fooBar = container.get(FooBar);
-//   const store = createStore(container, ServiceIdentifiers);
-//   expect(store.getState()).toEqual({
-//     FooToken: { count: 1 },
-//     FooToken1: { count: 1 },
-//     FooToken2: { count: 1 },
-//     FooToken3: { count: 1 },
-//     foo: { count: 1 },
-//   });
-//   fooBar.foos[0].increase();
-//   expect(store.getState()).toEqual({
-//     FooToken: { count: 2 },
-//     FooToken1: { count: 1 },
-//     FooToken2: { count: 1 },
-//     FooToken3: { count: 1 },
-//     foo: { count: 1 },
-//   });
-//   fooBar.foos[1].increase();
-//   expect(store.getState()).toEqual({
-//     FooToken: { count: 2 },
-//     FooToken1: { count: 2 },
-//     FooToken2: { count: 1 },
-//     FooToken3: { count: 1 },
-//     foo: { count: 1 },
-//   });
-// });
+    @action
+    increase() {
+      // inheritance
+      super.increase();
+      // change state
+      this.count0 += 1;
+      // call other action function
+      this.increase1();
+      // call unwrapped `@action` function
+      this.add(this.count);
+    }
 
-// test('module with stagedState about effect', () => {
-//   @injectable()
-//   class Foo0 {
-//     name = 'foo';
+    @action
+    increase1() {
+      this.count1 += 1;
+    }
+  }
 
-//     state = { count: 1, count1: 1 };
+  @injectable()
+  class FooBar {
+    constructor(public foo: Foo) {}
+  }
 
-//     @action
-//     increase() {
-//       this.state.count += 1;
-//     }
-//   }
+  const ServiceIdentifiers = new Map();
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules: [FooBar],
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const fooBar = container.get(FooBar);
+  const store = createStore(container, ServiceIdentifiers);
+  const subscribe = jest.fn();
+  store.subscribe(subscribe);
+  fooBar.foo.increase();
+  expect(fooBar.foo.count0).toBe(3);
+  expect(fooBar.foo.count1).toBe(2);
+  expect(fooBar.foo.count).toBe(2);
+  // merge the multi-actions changed states as one redux dispatch.
+  expect(subscribe.mock.calls.length).toBe(1);
+});
 
-//   @injectable()
-//   class Foo extends Foo0 {
-//     count = 1;
+test('module with multiple module injection with same module or others', () => {
+  @injectable()
+  class Foo {
+    name = 'foo';
 
-//     add(count: number) {
-//       this.count += count;
-//     }
+    @state
+    count = 1;
 
-//     @action
-//     increase() {
-//       super.increase();
-//       this.state.count += 1;
-//       this.increase1();
-//       this.add(this.count);
-//     }
+    @action
+    increase() {
+      this.count += 1;
+    }
+  }
 
-//     @action
-//     increase1() {
-//       this.state.count1 += 1;
-//     }
-//   }
+  @injectable()
+  class FooTest {
+    name = 'fooTest';
 
-//   @injectable()
-//   class FooBar {
-//     constructor(public foo: Foo) {}
-//   }
+    @state
+    count = 1;
+  }
 
-//   const ServiceIdentifiers = new Map();
-//   const container = createContainer({
-//     ServiceIdentifiers,
-//     modules: [FooBar],
-//     options: {
-//       defaultScope: 'Singleton',
-//     },
-//   });
-//   const fooBar = container.get(FooBar);
-//   const store = createStore(container, ServiceIdentifiers);
-//   const subscribe = jest.fn();
-//   store.subscribe(subscribe);
-//   fooBar.foo.increase();
-//   expect(fooBar.foo.state.count).toBe(3);
-//   expect(fooBar.foo.state.count1).toBe(2);
-//   expect(fooBar.foo.count).toBe(2);
-//   expect(subscribe.mock.calls.length).toBe(1);
-// });
+  @injectable()
+  class FooBar {
+    constructor(@multiInject('FooToken') public foos: Foo[], public foo: Foo) {}
+  }
+
+  const ServiceIdentifiers = new Map();
+  const modules = [
+    FooBar,
+    { provide: 'FooToken', useClass: Foo },
+    { provide: 'FooToken', useClass: Foo },
+    { provide: 'FooToken', useClass: FooTest },
+  ];
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules,
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const fooBar = container.get(FooBar);
+  const store = createStore(container, ServiceIdentifiers);
+  expect(store.getState()).toEqual({
+    FooToken: { count: 1 },
+    FooToken1: { count: 1 },
+    FooToken2: { count: 1 },
+    foo: { count: 1 },
+  });
+  fooBar.foos[0].increase();
+  expect(store.getState()).toEqual({
+    FooToken: { count: 2 },
+    FooToken1: { count: 1 },
+    FooToken2: { count: 1 },
+    foo: { count: 1 },
+  });
+  fooBar.foos[1].increase();
+  expect(store.getState()).toEqual({
+    FooToken: { count: 2 },
+    FooToken1: { count: 2 },
+    FooToken2: { count: 1 },
+    foo: { count: 1 },
+  });
+});
