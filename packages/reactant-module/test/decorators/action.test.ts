41354b743bbd5d2f1ea88e24fa9230fe53900cf4
unadlib <unadlib@gmail.com>
2020-04-03 21:55:49 +0800

    test(reactant-module): add `@autobind` testing

---
+++ packages/reactant-module/test/decorators/action.test.ts
@@ -0,0 +1,160 @@
+import { injectable, createContainer, state, createStore, action } from '../..';
+
+test('base `@action` decorate', () => {
+  @injectable()
+  class Counter {
+    @state
+    count = 0;
+
+    @action
+    increase() {
+      this.count += 1;
+    }
+
+    // TODO: decorate property function value.
+
+    // @action
+    // decrease = () => {
+    //   this.count -= 1;
+    // };
+  }
+  const ServiceIdentifiers = new Map();
+  const modules = [Counter];
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules,
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const counter = container.get(Counter);
+  const store = createStore(container, ServiceIdentifiers);
+  counter.increase();
+  expect(counter.count).toBe(1);
+  expect(Object.values(store.getState())).toEqual([{ count: 1 }]);
+});
+
+test('`@action` with base devOptions pararms', () => {
+  @injectable()
+  class Counter {
+    name = 'counter';
+
+    @state
+    count = 0;
+
+    @state
+    sum = { count: 0 };
+
+    @action
+    increase() {
+      this.sum.count += 1;
+    }
+
+    increase1() {
+      this.sum.count += 1;
+    }
+
+    increase2() {
+      this.count += 1;
+    }
+  }
+  const ServiceIdentifiers = new Map();
+  const modules = [Counter];
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules,
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const couter = container.get(Counter);
+  const store = createStore(
+    container,
+    ServiceIdentifiers,
+    undefined,
+    undefined,
+    undefined,
+    { autoFreeze: true }
+  );
+  expect(() => {
+    store.getState().counter.sum.count = 1;
+  }).toThrowError(/Cannot assign to read only property/);
+  couter.increase();
+  for (const fn of [
+    () => {
+      store.getState().counter.sum.count = 1;
+    },
+    () => couter.increase1(),
+    () => couter.increase2(),
+  ]) {
+    expect(fn).toThrowError(/Cannot assign to read only property/);
+  }
+});
+
+test('`@action` in inherited module with stagedState about more effects', () => {
+  @injectable()
+  class Foo0 {
+    name = 'foo';
+
+    @state
+    count0 = 1;
+
+    @state
+    count1 = 1;
+
+    @action
+    increase() {
+      this.count0 += 1;
+    }
+  }
+
+  @injectable()
+  class Foo extends Foo0 {
+    count = 1;
+
+    add(count: number) {
+      this.count += count;
+    }
+
+    @action
+    increase() {
+      // inheritance
+      super.increase();
+      // change state
+      this.count0 += 1;
+      // call other action function
+      this.increase1();
+      // call unwrapped `@action` function
+      this.add(this.count);
+    }
+
+    @action
+    increase1() {
+      this.count1 += 1;
+    }
+  }
+
+  @injectable()
+  class FooBar {
+    constructor(public foo: Foo) {}
+  }
+
+  const ServiceIdentifiers = new Map();
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules: [FooBar],
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const fooBar = container.get(FooBar);
+  const store = createStore(container, ServiceIdentifiers);
+  const subscribe = jest.fn();
+  store.subscribe(subscribe);
+  fooBar.foo.increase();
+  expect(fooBar.foo.count0).toBe(3);
+  expect(fooBar.foo.count1).toBe(2);
+  expect(fooBar.foo.count).toBe(2);
+  // merge the multi-actions changed states as one redux dispatch.
+  expect(subscribe.mock.calls.length).toBe(1);
+});
---
+++ packages/reactant-module/test/decorators/autobind.test.ts
@@ -0,0 +1,136 @@
+import {
+  injectable,
+  createContainer,
+  state,
+  createStore,
+  action,
+  autobind,
+} from '../..';
+
+test('base `@autobind` decorate', () => {
+  @injectable()
+  class Counter {
+    @state
+    count = 0;
+
+    @action
+    increase() {
+      this.count += 1;
+    }
+
+    @autobind
+    @action
+    decrease() {
+      this.count -= 1;
+    }
+
+    @autobind
+    run() {
+      this.increase();
+    }
+  }
+  const ServiceIdentifiers = new Map();
+  const modules = [Counter];
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules,
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const counter = container.get(Counter);
+  const store = createStore(container, ServiceIdentifiers);
+  const { increase, run, decrease } = counter;
+  expect(counter.count).toBe(0);
+  expect(() => {
+    increase();
+  }).toThrowError("Cannot read property 'Symbol(stagedState)' of undefined");
+  expect(counter.count).toBe(0);
+  run();
+  expect(counter.count).toBe(1);
+  decrease();
+  expect(counter.count).toBe(0);
+});
+
+test('`@autobind` decorate about inheritance', () => {
+  @injectable()
+  class BaseCounter {
+    @state
+    count = 0;
+
+    @action
+    increase() {
+      this.count += 1;
+    }
+
+    @autobind
+    @action
+    decrease() {
+      this.count -= 1;
+    }
+
+    @autobind
+    run() {
+      this.increase();
+    }
+  }
+
+  @injectable()
+  class Counter extends BaseCounter {
+    @autobind
+    decrease() {
+      super.decrease();
+    }
+
+    @autobind
+    run() {
+      super.run();
+      this.increase();
+      this.decrease();
+    }
+  }
+  const ServiceIdentifiers = new Map();
+  const modules = [Counter];
+  const container = createContainer({
+    ServiceIdentifiers,
+    modules,
+    options: {
+      defaultScope: 'Singleton',
+    },
+  });
+  const counter = container.get(Counter);
+  const store = createStore(container, ServiceIdentifiers);
+  const { run, decrease } = counter;
+  expect(counter.count).toBe(0);
+  decrease();
+  expect(counter.count).toBe(-1);
+  run();
+  expect(counter.count).toBe(0);
+});
+
+test('`@autobind` decorate error', () => {
+  expect(() => {
+    @injectable()
+    class Counter {
+      @state
+      count = 0;
+
+      // decorate error
+      @action
+      @autobind
+      errorDecrease() {
+        this.count -= 1;
+      }
+    }
+    const ServiceIdentifiers = new Map();
+    const modules = [Counter];
+    const container = createContainer({
+      ServiceIdentifiers,
+      modules,
+      options: {
+        defaultScope: 'Singleton',
+      },
+    });
+    const counter = container.get(Counter);
+  }).toThrowError(/decorate error with '@action'/);
+});
