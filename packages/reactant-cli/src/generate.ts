49599f7a8a63208e54914cd785693e2d3ed0f9c3
unadlib <unadlib@gmail.com>
2020-05-01 17:08:54 +0800

    improvement(reactant-cli): `generate` cmd

--- packages/reactant-cli/src/generate.ts
+++ packages/reactant-cli/src/generate.ts
@@ -3,7 +3,23 @@ import { Command } from 'commander';
 import path from 'path';
 import fs from 'fs-extra';
 import chalk from 'chalk';
-import { detectIsRootPath } from './utils';
+import { lookupRoot } from './utils';
+
+const templateTypeMap = {
+  s: 'service',
+  service: 'service',
+  v: 'view',
+  view: 'view',
+} as const;
+
+const supportLanguageMap = {
+  ts: 'typescript',
+  typescript: 'typescript',
+  js: 'javascript',
+  javascript: 'javascript',
+} as const;
+
+const supportLanguages = Array.from(new Set(Object.values(supportLanguageMap)));
 
 export const createGenerateCommand = (command: Command) => {
   command
@@ -15,72 +31,113 @@ export const createGenerateCommand = (command: Command) => {
     .option('-s, --skipTests', 'skip creating test files', true)
     .option(
       '-l, --language <language>',
-      'specify a file type(javascript/typescript)',
-      'typescript'
+      `specify a file type(${supportLanguages.join('/')})`,
+      supportLanguages[0]
     )
-    .action((templateType, { skipTests, language }, files) => {
-      // TODO: lookup root path.
-      const isRootPath = detectIsRootPath();
-      if (typeof files === 'undefined') {
-        console.error(chalk.red('The file name is required.'));
-        process.exit(1);
-      }
-      if (!['service', 'view'].includes(templateType)) {
-        console.error(
-          chalk.red(
-            `The template name should be 'service' or 'view', not '${templateType}'.`
-          )
-        );
-        process.exit(1);
-      }
-      const typeName = `${language === 'javascript' ? 'js' : 'ts'}${
-        templateType === 'view' ? 'x' : ''
-      }`;
-      const templateName = `template.${templateType}.${typeName}`;
-      const templatePath = path.resolve(
-        __dirname,
-        `../templates/${templateType}/${templateName}`
-      );
-      // TODO: `skipTests`
-      for (const file of files) {
+    .action(
+      (
+        type: keyof typeof templateTypeMap,
+        {
+          skipTests,
+          language,
+        }: { skipTests: boolean; language: keyof typeof supportLanguageMap },
+        files: string[]
+      ) => {
+        // TODO: lookup root path.
+        const currentPath = process.cwd();
+        let projectRootPath = currentPath;
         try {
-          // eslint-disable-next-line no-eval
-          eval(`var ${file};`);
+          projectRootPath = lookupRoot(currentPath);
         } catch (e) {
-          console.error(chalk.red(`${file} is not a valid name.`));
-          break;
-        }
-        const fileFullName = `${file}.${templateType}.${typeName}`;
-        const filePath = path.join(
-          process.cwd(),
-          isRootPath ? 'src' : '',
-          fileFullName
-        );
-        if (fs.existsSync(filePath)) {
-          console.error(chalk.red(`'${filePath}' file already exists.`));
+          console.log(
+            chalk.red('The command requires to be run in an Reactant project: ')
+          );
+          console.log(currentPath);
           process.exit(1);
         }
-        let templateString = fs.readFileSync(templatePath, {
-          encoding: 'utf8',
-        });
-        if (templateType === 'service') {
-          templateString = templateString.replace(
-            /TemplateService/g,
-            `${file}Service`
-          );
+        const isRootPath = projectRootPath === currentPath;
+        if (typeof files === 'undefined') {
+          console.error(chalk.red('The file name is required.'));
+          process.exit(1);
         }
-        if (templateType === 'view') {
-          templateString = templateString.replace(
-            /TemplateView/g,
-            `${file}View`
+        const templateType = templateTypeMap[type];
+        if (typeof templateType !== 'string') {
+          console.error(
+            chalk.red(
+              `The template name should be ${Object.keys(templateTypeMap).join(
+                ', '
+              )}, not '${type}'.`
+            )
           );
+          process.exit(1);
         }
-        fs.writeFileSync(filePath, templateString);
-        const stats = fs.statSync(filePath);
-        console.log(
-          chalk.cyan('Create'),
-          ` ${fileFullName}  (${stats.size} bytes)`
+
+        const templateLanguage = supportLanguageMap[language];
+        if (typeof templateLanguage !== 'string') {
+          console.error(
+            chalk.red(
+              `The template language should be ${supportLanguages.join(
+                ', '
+              )}, not '${language}'.`
+            )
+          );
+          process.exit(1);
+        }
+
+        const typeName = `${
+          templateLanguage === supportLanguageMap.javascript ? 'js' : 'ts'
+        }${templateType === 'view' ? 'x' : ''}`;
+        const templateName = `template.${templateType}.${typeName}`;
+        const templatePath = path.resolve(
+          __dirname,
+          `../templates/${templateType}/${templateName}`
         );
+        // TODO: implement `skipTests`
+        for (const file of files) {
+          try {
+            // eslint-disable-next-line no-eval
+            eval(`var ${file};`);
+          } catch (e) {
+            console.error(chalk.red(`${file} is not a valid name.`));
+            break;
+          }
+          const fileFullName = `${file}.${templateType}.${typeName}`;
+          // TODO: customized default source path
+          const projectRootDefaultSourcePath = path.join(
+            projectRootPath,
+            'src'
+          );
+          const filePath = path.join(
+            isRootPath ? projectRootDefaultSourcePath : currentPath,
+            fileFullName
+          );
+          if (fs.existsSync(filePath)) {
+            console.error(chalk.red(`'${filePath}' file already exists.`));
+            process.exit(1);
+          }
+          let templateString = fs.readFileSync(templatePath, {
+            encoding: 'utf8',
+          });
+          if (templateType === 'service') {
+            templateString = templateString.replace(
+              /TemplateService/g,
+              `${file}Service`
+            );
+          }
+          if (templateType === 'view') {
+            templateString = templateString.replace(
+              /TemplateView/g,
+              `${file}View`
+            );
+          }
+          fs.writeFileSync(filePath, templateString);
+          const stats = fs.statSync(filePath);
+          const relativePath = path.relative(projectRootPath, filePath);
+          console.log(
+            chalk.cyan('Create'),
+            ` ${relativePath}  (${stats.size} bytes)`
+          );
+        }
       }
-    });
+    );
 };
--- packages/reactant-cli/src/utils.ts
+++ packages/reactant-cli/src/utils.ts
@@ -2,11 +2,21 @@ import path from 'path';
 import fs from 'fs-extra';
 import { PackageJson } from './index';
 
-export const detectIsRootPath = () => {
-  const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+export const detectIsRootPath = (currentPath: string) => {
+  const packageJsonPath = path.resolve(currentPath, 'package.json');
   if (fs.existsSync(packageJsonPath)) {
     const packageJson: PackageJson = fs.readJsonSync(packageJsonPath);
     return !!packageJson.dependencies?.reactant;
   }
   return false;
+};
+
+export const lookupRoot = (currentPath: string): string => {
+  if (currentPath === '' || currentPath === '/') {
+    throw new Error('There are no Reactant projects');
+  }
+  if (!detectIsRootPath(currentPath)) {
+    return lookupRoot(path.resolve(currentPath, '..'));
+  }
+  return currentPath;
 };
