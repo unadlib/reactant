e50064a7d223547c5999b5a49dea6d5761c457c7
unadlib <unadlib@gmail.com>
2020-05-07 03:23:59 +0800

    fix(website): update project config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Reactant', // Title for your website.
   tagline: 'A framework for building React applications',
   url: 'https://unadlib.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/reactant/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
