d22c6e4cc3f1c7037d081803c6161cf4137bd216
unadlib <unadlib@gmail.com>
2020-05-25 00:31:30 +0800

    refactor(reactant-module): adjust testing

--- packages/reactant-module/test/decorators/action.test.ts
+++ packages/reactant-module/test/decorators/action.test.ts
@@ -1,179 +1,181 @@
 import { injectable, createContainer, state, createStore, action } from '../..';
 
-test('base `@action` decorate', () => {
-  @injectable()
-  class Counter {
-    @state
-    count = 0;
+describe('@action', () => {
+  test('base', () => {
+    @injectable()
+    class Counter {
+      @state
+      count = 0;
 
-    @action
-    increase() {
-      this.count += 1;
+      @action
+      increase() {
+        this.count += 1;
+      }
     }
-  }
-  const ServiceIdentifiers = new Map();
-  const modules = [Counter];
-  const container = createContainer({
-    ServiceIdentifiers,
-    modules,
-    options: {
-      defaultScope: 'Singleton',
-    },
+    const ServiceIdentifiers = new Map();
+    const modules = [Counter];
+    const container = createContainer({
+      ServiceIdentifiers,
+      modules,
+      options: {
+        defaultScope: 'Singleton',
+      },
+    });
+    const counter = container.get(Counter);
+    const store = createStore(modules, container, ServiceIdentifiers);
+    counter.increase();
+    expect(counter.count).toBe(1);
+    expect(Object.values(store.getState())).toEqual([{ count: 1 }]);
   });
-  const counter = container.get(Counter);
-  const store = createStore(modules, container, ServiceIdentifiers);
-  counter.increase();
-  expect(counter.count).toBe(1);
-  expect(Object.values(store.getState())).toEqual([{ count: 1 }]);
-});
 
-test('`@action` with base devOptions pararms', () => {
-  @injectable()
-  class Counter {
-    name = 'counter';
+  test('enable `autoFreeze` in devOptions', () => {
+    @injectable()
+    class Counter {
+      name = 'counter';
 
-    @state
-    count = 0;
+      @state
+      count = 0;
 
-    @state
-    sum = { count: 0 };
+      @state
+      sum = { count: 0 };
 
-    @action
-    increase() {
-      this.sum.count += 1;
+      @action
+      increase() {
+        this.sum.count += 1;
+      }
+
+      increase1() {
+        this.sum.count += 1;
+      }
+
+      increase2() {
+        this.count += 1;
+      }
     }
-
-    increase1() {
-      this.sum.count += 1;
-    }
-
-    increase2() {
-      this.count += 1;
-    }
-  }
-  const ServiceIdentifiers = new Map();
-  const modules = [Counter];
-  const container = createContainer({
-    ServiceIdentifiers,
-    modules,
-    options: {
-      defaultScope: 'Singleton',
-    },
-  });
-  const counter = container.get(Counter);
-  const store = createStore(
-    modules,
-    container,
-    ServiceIdentifiers,
-    undefined,
-    undefined,
-    { autoFreeze: true }
-  );
-  expect(() => {
-    store.getState().counter.sum.count = 1;
-  }).toThrowError(/Cannot assign to read only property/);
-  counter.increase();
-  for (const fn of [
-    () => {
+    const ServiceIdentifiers = new Map();
+    const modules = [Counter];
+    const container = createContainer({
+      ServiceIdentifiers,
+      modules,
+      options: {
+        defaultScope: 'Singleton',
+      },
+    });
+    const counter = container.get(Counter);
+    const store = createStore(
+      modules,
+      container,
+      ServiceIdentifiers,
+      undefined,
+      undefined,
+      { autoFreeze: true }
+    );
+    expect(() => {
       store.getState().counter.sum.count = 1;
-    },
-    () => counter.increase1(),
-    () => counter.increase2(),
-  ]) {
-    expect(fn).toThrowError(/Cannot assign to read only property/);
-  }
-});
-
-test('`@action` in inherited module with stagedState about more effects', () => {
-  @injectable()
-  class Foo0 {
-    name = 'foo';
-
-    @state
-    count0 = 1;
-
-    @state
-    count1 = 1;
-
-    @action
-    increase() {
-      this.count0 += 1;
+    }).toThrowError(/Cannot assign to read only property/);
+    counter.increase();
+    for (const fn of [
+      () => {
+        store.getState().counter.sum.count = 1;
+      },
+      () => counter.increase1(),
+      () => counter.increase2(),
+    ]) {
+      expect(fn).toThrowError(/Cannot assign to read only property/);
     }
-
-    @action
-    decrease() {
-      this.count0 -= 1;
-    }
-
-    @action
-    decrease1() {
-      this.count0 -= 1;
-    }
-  }
-
-  @injectable()
-  class Foo extends Foo0 {
-    count = 1;
-
-    add(count: number) {
-      this.count += count;
-    }
-
-    @action
-    increase() {
-      // inheritance
-      super.increase();
-      // change state
-      this.count0 += 1;
-      // call other action function
-      this.increase1();
-      // call unwrapped `@action` function
-      this.add(this.count);
-    }
-
-    @action
-    increase1() {
-      this.count1 += 1;
-    }
-
-    @action
-    decrease() {
-      super.decrease();
-      this.count0 -= 1;
-    }
-
-    decrease1() {
-      super.decrease1();
-    }
-  }
-
-  @injectable()
-  class FooBar {
-    constructor(public foo: Foo) {}
-  }
-
-  const ServiceIdentifiers = new Map();
-  const modules = [FooBar];
-  const container = createContainer({
-    ServiceIdentifiers,
-    modules,
-    options: {
-      defaultScope: 'Singleton',
-    },
   });
-  const fooBar = container.get(FooBar);
-  const store = createStore(modules, container, ServiceIdentifiers);
-  const subscribe = jest.fn();
-  store.subscribe(subscribe);
-  fooBar.foo.increase();
-  expect(fooBar.foo.count0).toBe(3);
-  expect(fooBar.foo.count1).toBe(2);
-  expect(fooBar.foo.count).toBe(2);
-  // merge the multi-actions changed states as one redux dispatch.
-  expect(subscribe.mock.calls.length).toBe(1);
-  // inheritance
-  fooBar.foo.decrease();
-  expect(fooBar.foo.count0).toBe(1);
-  fooBar.foo.decrease1();
-  expect(fooBar.foo.count0).toBe(0);
+
+  test('inherited module with stagedState about more effects', () => {
+    @injectable()
+    class Foo0 {
+      name = 'foo';
+
+      @state
+      count0 = 1;
+
+      @state
+      count1 = 1;
+
+      @action
+      increase() {
+        this.count0 += 1;
+      }
+
+      @action
+      decrease() {
+        this.count0 -= 1;
+      }
+
+      @action
+      decrease1() {
+        this.count0 -= 1;
+      }
+    }
+
+    @injectable()
+    class Foo extends Foo0 {
+      count = 1;
+
+      add(count: number) {
+        this.count += count;
+      }
+
+      @action
+      increase() {
+        // inheritance
+        super.increase();
+        // change state
+        this.count0 += 1;
+        // call other action function
+        this.increase1();
+        // call unwrapped `@action` function
+        this.add(this.count);
+      }
+
+      @action
+      increase1() {
+        this.count1 += 1;
+      }
+
+      @action
+      decrease() {
+        super.decrease();
+        this.count0 -= 1;
+      }
+
+      decrease1() {
+        super.decrease1();
+      }
+    }
+
+    @injectable()
+    class FooBar {
+      constructor(public foo: Foo) {}
+    }
+
+    const ServiceIdentifiers = new Map();
+    const modules = [FooBar];
+    const container = createContainer({
+      ServiceIdentifiers,
+      modules,
+      options: {
+        defaultScope: 'Singleton',
+      },
+    });
+    const fooBar = container.get(FooBar);
+    const store = createStore(modules, container, ServiceIdentifiers);
+    const subscribe = jest.fn();
+    store.subscribe(subscribe);
+    fooBar.foo.increase();
+    expect(fooBar.foo.count0).toBe(3);
+    expect(fooBar.foo.count1).toBe(2);
+    expect(fooBar.foo.count).toBe(2);
+    // merge the multi-actions changed states as one redux dispatch.
+    expect(subscribe.mock.calls.length).toBe(1);
+    // inheritance
+    fooBar.foo.decrease();
+    expect(fooBar.foo.count0).toBe(1);
+    fooBar.foo.decrease1();
+    expect(fooBar.foo.count0).toBe(0);
+  });
 });
--- packages/reactant-module/test/decorators/state.test.ts
+++ packages/reactant-module/test/decorators/state.test.ts
@@ -1,129 +1,64 @@
-import {
-  injectable,
-  createContainer,
-  createStore,
-  action,
-  state,
-  createState,
-  ReactantAction,
-} from '../..';
+import { injectable, createContainer, createStore, action, state } from '../..';
 
-test('base `@state`', () => {
-  @injectable()
-  class Counter {
-    @state
-    count1 = 0;
+describe('@state', () => {
+  test('base', () => {
+    @injectable()
+    class Counter {
+      @state
+      count = 0;
 
-    @state
-    others = {
-      list: [] as number[],
-    };
-
-    @state
-    count = createState<number, ReactantAction>((_state = 0, _action) =>
-      _action.type === (this as any).name ? _action.state.count : _state
-    );
-
-    @action
-    increase() {
-      this.count += 1;
+      @action
+      increase() {
+        this.count += 1;
+      }
     }
-
-    @action
-    increase1() {
-      this.count1 += 1;
-    }
-
-    @action
-    add() {
-      this.others.list.push(this.others.list.length);
-    }
-  }
-  const ServiceIdentifiers = new Map();
-  const modules = [Counter];
-  const container = createContainer({
-    ServiceIdentifiers,
-    modules,
-    options: {
-      defaultScope: 'Singleton',
-    },
+    const ServiceIdentifiers = new Map();
+    const modules = [Counter];
+    const container = createContainer({
+      ServiceIdentifiers,
+      modules,
+      options: {
+        defaultScope: 'Singleton',
+      },
+    });
+    const counter = container.get(Counter);
+    const store = createStore(modules, container, ServiceIdentifiers);
+    expect(counter.count).toBe(0);
+    expect(Object.values(store.getState())).toEqual([{ count: 0 }]);
+    counter.increase();
+    expect(counter.count).toBe(1);
   });
-  const counter = container.get(Counter);
-  const store = createStore(modules, container, ServiceIdentifiers);
-  expect(counter.count).toBe(0);
-  expect(Object.values(store.getState())).toEqual([
-    { count: 0, count1: 0, others: { list: [] } },
-  ]);
-  counter.increase();
-  expect(counter.count).toBe(1);
-  counter.add();
-  expect(counter.others.list).toEqual([0]);
-  expect(Object.values(store.getState())).toEqual([
-    { count: 1, count1: 0, others: { list: [0] } },
-  ]);
-  counter.increase1();
-  expect(counter.count1).toBe(1);
-});
 
-test('`@state` about inheritance', () => {
-  @injectable()
-  class BaseCounter {
-    @state
-    count = 0;
+  test('inheritance', () => {
+    @injectable()
+    class BaseCounter {
+      @state
+      count = 0;
 
-    @state
-    state = {
-      test: 'test',
-    };
-
-    @action
-    add() {
-      this.state.test += '1';
+      @action
+      increase() {
+        this.count += 1;
+      }
     }
 
-    @action
-    increase() {
-      this.count += 1;
+    @injectable()
+    class Counter extends BaseCounter {
+      @state
+      count = 10;
     }
-  }
-
-  @injectable()
-  class Counter extends BaseCounter {
-    @state
-    count = 10;
-  }
-  const ServiceIdentifiers = new Map();
-  const modules = [Counter];
-  const container = createContainer({
-    ServiceIdentifiers,
-    modules,
-    options: {
-      defaultScope: 'Singleton',
-    },
+    const ServiceIdentifiers = new Map();
+    const modules = [Counter];
+    const container = createContainer({
+      ServiceIdentifiers,
+      modules,
+      options: {
+        defaultScope: 'Singleton',
+      },
+    });
+    const counter = container.get(Counter);
+    createStore(modules, container, ServiceIdentifiers);
+    expect(counter.count).toBe(10);
+    counter.increase();
+    expect(counter.count).toBe(11);
   });
-  const counter = container.get(Counter);
-  const store = createStore(modules, container, ServiceIdentifiers);
-  expect(counter.count).toBe(10);
-  counter.increase();
-  expect(counter.count).toBe(11);
-  counter.add();
-  expect(counter.state.test).toBe('test1');
-
-  // console.log(store.getState());
-
-  const ServiceIdentifiers1 = new Map();
-  const container1 = createContainer({
-    ServiceIdentifiers: ServiceIdentifiers1,
-    modules,
-    options: {
-      defaultScope: 'Singleton',
-    },
-  });
-  const counter1 = container1.get(Counter);
-  const store1 = createStore(modules, container1, ServiceIdentifiers1);
-  expect(counter1.count).toBe(10);
-  counter1.increase();
-  expect(counter1.count).toBe(11);
-  counter1.add();
-  expect(counter1.state.test).toBe('test1');
 });
