eb2792e5a25c1cc6927f0b19ea296566895ed7f4
unadlib <unadlib@gmail.com>
2020-07-04 01:26:45 +0800

    docs(reactant): add `testBed` doc

--- packages/reactant/src/testBed.ts
+++ packages/reactant/src/testBed.ts
@@ -2,6 +2,34 @@ import { PartialKeys } from 'reactant-module';
 import { createApp } from './createApp';
 import { Config, App } from './interfaces';
 
+/**
+ * **Description:**
+ *
+ * You can use `testBed` to build your test code without `render`(`render` function is optional.).
+ *
+ * **Example:**
+ *
+ * ```ts
+ * @injectable()
+ * class Bar {
+ *   getValue() {
+ *     return 'bar';
+ *   }
+ * }
+ *
+ * @injectable()
+ * class Foo {
+ *   constructor(public bar: Bar) {}
+ * }
+ *
+ * const foo = testBed({
+ *   modules: [{ provide: Foo, useValue: { getValue: () => 'test' } }],
+ *   main: Foo,
+ * });
+ *
+ * expect(foo.instance.bar.getValue).toBe('test');
+ * ```
+ */
 function testBed<T>(config: PartialKeys<Config<T>, 'render'>): App<T> {
   return createApp<T>({
     ...config,
