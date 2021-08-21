7e90ba771581c9e63702e314c3e2cab53335334e
unadlib <unadlib@gmail.com>
2021-08-21 14:56:12 +0800

    test(reactant-share): add testing

---
+++ packages/reactant-share/test/case.test.ts
@@ -0,0 +1,115 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { mockPairPorts, createTransport } from 'data-transport';
+import { createSharedApp, injectable, state, action, proxify } from '..';
+
+@injectable()
+class Counter {
+  name = 'counter';
+
+  @state
+  count = 0;
+
+  @action
+  _increase() {
+    this.count += 1;
+  }
+
+  @proxify
+  async increase() {
+    this._increase();
+  }
+}
+
+test('base: sync up full state', async () => {
+  const ports = mockPairPorts();
+
+  const server = await createSharedApp({
+    modules: [],
+    main: Counter,
+    render: () => {},
+    share: {
+      name: 'counter',
+      type: 'Base',
+      port: 'server',
+      transports: {
+        server: createTransport('Base', ports[0]),
+      },
+    },
+  });
+
+  await server.instance.increase();
+  expect(server.instance.count).toBe(1);
+  await server.instance.increase();
+  expect(server.instance.count).toBe(2);
+
+  const client0 = await createSharedApp({
+    modules: [],
+    main: Counter,
+    render: () => {},
+    share: {
+      name: 'counter',
+      type: 'Base',
+      port: 'client',
+      transports: {
+        client: createTransport('Base', ports[1]),
+      },
+    },
+  });
+  expect(client0.instance.count).toBe(2);
+  expect(server.instance.count).toBe(2);
+
+  await client0.instance.increase();
+  expect(server.instance.count).toBe(3);
+  expect(client0.instance.count).toBe(3);
+
+  await server.instance.increase();
+  expect(server.instance.count).toBe(4);
+  expect(client0.instance.count).toBe(4);
+});
+
+test('sync up full state and update from server', async () => {
+  const ports = mockPairPorts();
+
+  const server = await createSharedApp({
+    modules: [],
+    main: Counter,
+    render: () => {},
+    share: {
+      name: 'counter',
+      type: 'Base',
+      port: 'server',
+      transports: {
+        server: createTransport('Base', ports[0]),
+      },
+    },
+  });
+
+  await server.instance.increase();
+  expect(server.instance.count).toBe(1);
+  await server.instance.increase();
+  expect(server.instance.count).toBe(2);
+
+  const client0 = await createSharedApp({
+    modules: [],
+    main: Counter,
+    render: () => {},
+    share: {
+      name: 'counter',
+      type: 'Base',
+      port: 'client',
+      transports: {
+        client: createTransport('Base', ports[1]),
+      },
+    },
+  });
+  expect(client0.instance.count).toBe(2);
+  expect(server.instance.count).toBe(2);
+
+  await server.instance.increase();
+  expect(server.instance.count).toBe(3);
+  expect(client0.instance.count).toBe(3);
+
+  await server.instance.increase();
+  expect(server.instance.count).toBe(4);
+  expect(client0.instance.count).toBe(4);
+});
