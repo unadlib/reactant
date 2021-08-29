da09dda5798bcef61db1b605039261b5ada03424
unadlib <unadlib@gmail.com>
2021-08-29 17:22:38 +0800

    fix(reactant-template): update ServiceWorker ts template

--- packages/reactant-template/templates/service-worker/typescript/template/src/worker.ts
+++ packages/reactant-template/templates/service-worker/typescript/template/src/worker.ts
@@ -1,10 +1,6 @@
 import { createSharedApp } from 'reactant-share';
 import { AppView } from './app.view';
 
-// follow issue: https://github.com/microsoft/TypeScript/issues/20595
-// workaround: `tsc --skipLibCheck`.
-declare var self: ServiceWorkerGlobalScope;
-
 createSharedApp({
   modules: [],
   main: AppView,
