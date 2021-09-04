89a00dd556382aee4a3d7b920fc6e30c3e9a36fd
unadlib <unadlib@gmail.com>
2021-09-04 19:44:20 +0800

    test(reactant): add testing about testBed

--- packages/reactant/test/testBed.test.ts
+++ packages/reactant/test/testBed.test.ts
@@ -1,4 +1,4 @@
-import { injectable, testBed } from '..';
+import { injectable, testBed, ViewModule } from '..';
 
 describe('testBed', () => {
   test('mock with useValue', () => {
@@ -24,7 +24,45 @@ describe('testBed', () => {
       main: Bar,
       modules: [{ provide: Foo, useValue: { getValue: () => 'test' } }],
     });
-
     expect(bar.instance.getValue()).toBe('testBar');
+    expect(() => {
+      bar.bootstrap(document.createElement('div'));
+    }).toThrow(`Main module should be a 'ViewModule'.`);
+  });
+
+  test('mock with custom render', () => {
+    @injectable()
+    class Foo {
+      getValue() {
+        return 'foo';
+      }
+    }
+
+    @injectable()
+    class Bar extends ViewModule {
+      constructor(public foo: Foo) {
+        super();
+      }
+
+      getValue() {
+        return `${this.foo.getValue()}Bar`;
+      }
+
+      component() {
+        return null;
+      }
+    }
+
+    const bar = testBed({
+      main: Bar,
+      modules: [{ provide: Foo, useValue: { getValue: () => 'test' } }],
+      render: null as any,
+    });
+
+    const spy = jest.spyOn(console, 'log').mockImplementation();
+    bar.bootstrap(document.createElement('div'));
+    expect(spy.mock.calls.slice(-1)).toEqual([
+      ['No render function is configured.'],
+    ]);
   });
 });
