c8da0d77fd207b3cfe533a526dc4ba66265387f6
unadlib <unadlib@gmail.com>
2022-04-12 19:46:09 +0800

    fix(reactant-module): fix lazy decorator check error

--- packages/reactant-module/src/decorators/lazy.ts
+++ packages/reactant-module/src/decorators/lazy.ts
@@ -1,6 +1,6 @@
 import { getLazyDecorator, ServiceIdentifier } from 'reactant-di';
 
-import { containerKey } from '../constants';
+import { containerKey, storeKey } from '../constants';
 import { PropertyDescriptor, Service } from '../interfaces';
 
 type Lazy = (
@@ -18,12 +18,11 @@ export const lazy: Lazy = getLazyDecorator(
       const services = target![containerKey]!.getAll(serviceIdentifier);
       return services.length === 1 ? services[0] : services;
     } catch (e) {
-      // TODO: should only check after init
-      // if (process.env['NODE_ENV'] !== 'production') {
-      //   console.warn(
-      //     `Failed to get instance of lazy loading module ${serviceIdentifier.toString()}.`
-      //   );
-      // }
+      if (__DEV__ && target?.[storeKey]) {
+        console.warn(
+          `Failed to get instance of lazy loading module ${serviceIdentifier.toString()}.`
+        );
+      }
     }
     return null;
   }
