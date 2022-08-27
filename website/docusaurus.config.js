af3425d858bfb1ef15cb2f9af32de1ffff3baddc
unadlib <unadlib@gmail.com>
2022-08-28 04:18:37 +0800

    docs(website): update website config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -40,14 +40,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/unadlib/reactant/tree/master/website/docs/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/unadlib/reactant/tree/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
