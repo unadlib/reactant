62c12e9a32ff4c4166b56d3d1e68d42873d0bacb
unadlib <unadlib@gmail.com>
2022-08-28 04:20:33 +0800

    docs(website): update website config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -39,15 +39,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/unadlib/reactant/tree/master/website/docs/',
+          editUrl: 'https://github.com/unadlib/reactant/tree/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/unadlib/reactant/tree/master/website/blog/',
+          editUrl: 'https://github.com/unadlib/reactant/tree/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
