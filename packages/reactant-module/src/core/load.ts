a671e5cfbc2468ca2980a6bd9b3e4c4afa45d2ff
unadlib <unadlib@gmail.com>
2024-01-10 23:34:53 +0800

    fix(reacant): fix dynamic loading issue

--- packages/reactant-module/src/core/load.ts
+++ packages/reactant-module/src/core/load.ts
@@ -1,9 +1,19 @@
 import { Load } from '../interfaces';
 import { loaderKey } from '../constants';
+import { subscribe } from './subscribe';
 
 const load: Load = (service, options) => {
   return new Promise((resolve) => {
-    service[loaderKey]!(options, resolve);
+    if (!service[loaderKey]) {
+      const unsubscribe = subscribe(service, () => {
+        if (service[loaderKey]) {
+          unsubscribe();
+          service[loaderKey](options, resolve);
+        }
+      });
+      return;
+    }
+    service[loaderKey](options, resolve);
   });
 };
 
